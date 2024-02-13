11a05f7f861ee3434df10159622d5cc26e63ad59
root <root@DESKTOP-038UK9R.localdomain>
2024-02-13 10:46:49 +0800

    2024-02-13

--- not_logged_in.php
+++ not_logged_in.php
@@ -17,16 +17,31 @@ if (isset($login)) {
 ?>
 
 <!-- login form box -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 <form method="post" action="logged_in.php" name="loginform">
 
-    <label for="login_input_username">用户名:</label>
-    <input id="login_input_username" class="login_input" type="text" name="user_name" required />
+<label for="login_input_username">用户名:</label>
+<input id="login_input_username" class="login_input" type="text" name="user_name" required />
 
-    <label for="login_input_password">密码:</label>
-    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
+<label for="login_input_password">密码:</label>
+<input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
 
-    <input type="submit"  name="login" value="登陆" />
+<input type="submit"  name="login" value="登陆" />
 
 </form>
 
 <a href="register.php">注册</a>
+
+
+</body>
+</html>
+
--- register2.php
+++ register2.php
@@ -14,25 +14,39 @@ if (isset($registration)) {
 }
 ?>
 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
 <!-- register form -->
 <form method="post" action="register.php" name="registerform">
 
-    <!-- the user name input field uses a HTML5 pattern check -->
-    <label for="login_input_username">用户名(只能是字母或数字)</label>
-    <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
+<!-- the user name input field uses a HTML5 pattern check -->
+<label for="login_input_username">用户名(只能是字母或数字)</label>
+<input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
 
-    <!-- the email input field uses a HTML5 email type check -->
-    <label for="login_input_email">邮箱</label>
-    <input id="login_input_email" class="login_input" type="email" name="user_email" required />
+<!-- the email input field uses a HTML5 email type check -->
+<label for="login_input_email">邮箱</label>
+<input id="login_input_email" class="login_input" type="email" name="user_email" required />
 
-    <label for="login_input_password_new">密码,最少6个字符</label>
-    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
+<label for="login_input_password_new">密码,最少6个字符</label>
+<input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
 
-    <label for="login_input_password_repeat">重复密码</label>
-    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
-    <input type="submit"  name="register" value="注册" />
+<label for="login_input_password_repeat">重复密码</label>
+<input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
+<input type="submit"  name="register" value="注册" />
 
 </form>
 
-<!-- backlink -->
 <a href="index.php">回到登陆主页</a>
+
+
+</head>
+<body>
+    
+</body>
+</html>
